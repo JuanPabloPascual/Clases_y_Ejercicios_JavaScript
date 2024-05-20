761e6de7380536963771203fd0eb2e78b78409b7
juampi.pascual <jppascualiramain@gmail.com>
2024-05-19 21:22:27 -0300

    mini arreglo

--- script2.js
+++ script2.js
@@ -1,17 +1,17 @@
-/* 
+/*  Ejercicios del 13 de Mayo
+
 Escribe un condicional que verifique si una variable llamada "tieneHijos" es "true". 
 Si es "true", imprime "Tienes hijos" en la consola. Si es "false", imprime "No tienes hijos" en la consola. 
 
 Escribe un condicional que verifique si una variable llamada "genero" es "hombre" o "mujer". 
 Si es "hombre", imprime "Eres hombre" en la consola. Si es "mujer", imprime "Eres mujer" en la consola.
-
 que el usuario realice la carga de un dato por teclado dicho dato será el lado de un cuadrado,
 para luego  mostrar por pantalla el perímetro del mismo .
 
-hacer un programa donde se ingresan cuatro números, 
+Hacer un programa donde se ingresan cuatro números, 
 calcular e informar la suma de los dos primeros y la multiplicación del tercero y el cuarto.
 
-Se ingresa  un valor entero, mostrar un mensaje que indique si el número es positivo, cero o negativo
+Se ingresa  un valor entero, mostrar un mensaje que indique si el número es positivo, cero o negativo.
 
 Escribe una expresión que use el operador "&&" (and lógico) 
 para verificar si una variable llamada "edad" es mayor o igual a 18 y si una variable llamada "tienePermiso" es "true". 
@@ -20,10 +20,5 @@ Si alguna de las dos condiciones no se cumple, imprime "No puedes conducir" en l
 
 
 */
-
-
-
-
-
 
 
