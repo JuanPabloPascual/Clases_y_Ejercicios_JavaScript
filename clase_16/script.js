490b92ef3db73ceb95a025d55ecf858bfe941378
juampi.pascual <jppascualiramain@gmail.com>
2024-05-22 18:38:45 -0300

    intento de ejercicios

---
+++ clase_16/script.js
@@ -0,0 +1,101 @@
+/* Solicitar al usuario un texto y 
+
+verificar si se trata de una URL con certificado ssl Secure Sockets Layer (Capa de sockets seguros),
+
+Si cuenta con con https:// decir por consola "la url ingresada, cuenta con certificado ssl"
+Si no lo tiene pero si tiene http:// "la url ingresada no cuenta con certificado ssl"
+Si no posee ninguno decir "no has ingresado una url valida" */
+
+
+/* let texto = prompt('Ingrese texto')
+
+function verificar(url){
+    if (url.indexof(texto = url));
+    console.log('la url ingresada, cuenta con certificado ssl')
+}
+else{
+    console.log('a url ingresada no cuenta con certificado ssl');
+} */
+
+
+
+/* Dado los siguientes texto
+"hola%20como%20estas,%20todo%bien?"
+"no%20me%20siento%20bien"
+"que%20mal"
+
+Descifrar el codigo y mostrarlo por consola: "El codigo descifrado es: " + codigoDescifrado
+
+Averiguar la cantidad de caracteres y en base a el numero de caracteres mostrar
+
+"el mensaje es corto" entre 1 y 8 caracteres
+"el mensaje es mediano" entre 9 y 18
+"el mensaje es largo"  mas de 18
+
+Ademas si el mensaje cuenta con ",", "@", "-" debera decir, "el mensaje es complejo", sino decir
+
+"el mensaje no es complejo" */
+
+
+/* const text1 = "hola%20como%20estas,%20todo%bien?";
+const text2 = "no%20me%20siento%20bien";
+const text3 = "que%20mal";
+
+function codigoDescifrado(text) {
+    method replace por espacios
+    console log para mostrar mensajes
+
+    if y else para ver la longitud
+
+    y mostrar la complejidad al final con console log
+
+} */
+
+
+
+/* Necesitamos un programa que pida ingresar la distancia de un recorrido, mediante el mensaje:
+
+Ingresá la distancia del recorrido
+Con esta información, el programa deberá calcular cuánto tiempo tardaría en completar 
+el recorrido en distintos medios de transporte y luego mostrarlo mediante el siguiente mensaje: 
+
+Para la distancia {distancia} km en bicicleta el tiempo de viaje es {resultadoEnBicicleta} hora/s, 
+
+a pie {resultadoAPie} hora/s y 
+
+en auto {resultadoEnAuto} hora/s
+
+Las velocidades de los medio de transporte son:
+
+a pie : 5 km/ hs
+bicicleta : 10 km/ hs
+auto : 50 km/hs
+
+let distancia = prompt('Ingrese distancia del recorrido que desea realizar')
+let velocidadBicicleta = 10
+let velocidadAPie = 5
+let velocidadAuto = 50
+
+return resultadoEnBicicleta = distancia*velocidadBicicleta
+
+Cuanto tiempo tardo en hace 100km
+
+Si 5 km los hago en 60 minutos
+
+100*60 = 6000
+
+6000 = 60 hrs
+
+resultadoEnBicicleta = (distancia.velocidadBicicleta)
+resultadoAPie = (distancia.velocidadAPie)
+resultadoEnAuto = (distancia.velocidadAuto)
+
+
+alert('El Perimetro del cuadrado es: ' + perimetro )
+
+
+
+
+
+
+*/
