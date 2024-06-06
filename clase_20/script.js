7319c2ddc2c3def764d89f95b16b66959cb9f0ed
juampi.pascual <jppascualiramain@gmail.com>
2024-06-05 22:06:24 -0300

    clase_20

---
+++ clase_20/script.js
@@ -0,0 +1,25 @@
+/* Eventos */
+
+/* function alertar(){
+    alert('hola pepe')
+    
+} */
+
+
+
+//  FUNCIONES EN FLECHA ARROW
+
+/* function hacerTarea () {
+    let tipo = prompt('que tipo de tarea quieres hacer')
+    guardarTarea(tipo)
+    return tipo
+}
+
+const hacerTarea = () => {
+    let tipo = prompt('¿Qué tipo de tarea quieres hacer?');
+    guardarTarea(tipo);
+    return tipo;
+}
+
+const calcularIVa = (iva) => iva * 0.21; */
+
